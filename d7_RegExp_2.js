3cf994e9a613cb5fe373234e4d288a1bd4758034
Gizmo <nalanbilgin@gmail.com>
2024-12-30 16:08:32 +0200

    Added solution d7_RegExp_2

---
+++ d7_RegExp_2.js
@@ -0,0 +1,50 @@
+// Provided
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Solution
+function regexVar() {
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.',
+   * followed by one or more letters.
+   */
+  const re = /^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[A-Za-z]+$/;
+
+  /*
+   * Do not remove the return statement
+   */
+  return re;
+}
+
+// Provided
+function main() {
+  const re = regexVar();
+  const s = readLine();
+
+  console.log(!!s.match(re));
+}
