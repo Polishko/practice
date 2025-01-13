b6bb3a63e2f12e4d8757d74b2cdc3110f32c09e8
Gizmo <nalanbilgin@gmail.com>
2025-01-13 13:35:04 +0200

    Added solution

---
+++ HRProblemSolving/salesByMatch.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+//Solution start
+function sockMerchant(n, ar) {
+  // Write your code here
+  let sockPairs = {};
+  let count = 0;
+
+  for (let sock of ar) {
+    sockPairs[sock] = (sockPairs[sock] || 0) + 1;
+
+    if (sockPairs[sock] === 2) {
+      count++;
+      sockPairs[sock] = 0;
+    }
+  }
+
+  return count;
+}
+//Solution end
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const ar = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arTemp) => parseInt(arTemp, 10));
+
+  const result = sockMerchant(n, ar);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
