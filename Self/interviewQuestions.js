52710a5d76d9286ff75a8babd576f7cf01981da1
Gizmo <nalanbilgin@gmail.com>
2025-01-13 16:48:00 +0200

    Added some test practice examples

--- Self/interviewQuestions.js
+++ Self/interviewQuestions.js
@@ -15,10 +15,34 @@ console.log(isPalindrome("Racecar")); // true
 console.log(isPalindrome("hello")); // false
 
 // Random background color on button click
-const buttonElement = document.querySelector("button");
-const colors = ["blue", "red", "green", "gray", "purple"];
+// const buttonElement = document.querySelector("button");
+// const colors = ["blue", "red", "green", "gray", "purple"];
 
-buttonElement.addEventListener("click", function () {
-  document.body.style.background =
-    colors[Math.floor(Math.random() * colors.length)];
-});
+// buttonElement.addEventListener("click", function () {
+//   document.body.style.background =
+//     colors[Math.floor(Math.random() * colors.length)];
+// });
+
+// Remember map exact syntaxis
+function squareNumbers(arr) {
+  return arr.map((num) => num ** 2);
+}
+
+let arr = [1, 2, 3, 4];
+console.log(squareNumbers(arr));
+
+// typeof null and NAN
+console.log(typeof null);
+console.log(typeof NAN);
+
+// Create button
+// const buttonElement = document.createElement('button');
+// buttonElement.textContent = 'Click me'; // Add button text
+// const bodyElement = document.querySelector('body');
+// bodyElement.appendChild(buttonElement);
+
+// buttonElement.addEventListener('click', function () {
+//     const pElement = document.createElement('p');
+//     pElement.textContent = 'Button clicked!';
+//     bodyElement.appendChild(pElement);
+// });
