82b48da3b1f5a6a222b01c029d33f7219878bfd3
Gizmo <nalanbilgin@gmail.com>
2025-01-13 09:04:15 +0200

    Added improved version

--- HRProblemSolving/migratoryBirds.js
+++ HRProblemSolving/migratoryBirds.js
@@ -28,23 +28,53 @@ function readLine() {
  * The function is expected to return an INTEGER.
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
-
+// Solution start
 function migratoryBirds(arr) {
-  // Write your code here
   let birdTypes = {};
-  for (let type of arr) {
-    const birdType = new String(type);
-    if (!birdTypes.hasOwnProperty(type)) {
-      birdTypes[type] = 0;
-    }
+  let maxCount = 0;
+  let mostFrequentBird = Infinity;
 
-    birdTypes[birdType] += 1;
+  for (let type of arr) {
+    birdTypes[type] = (birdTypes[type] || 0) + 1;
+
+    if (
+      birdTypes[type] > maxCount ||
+      (birdTypes[type] === maxCount && type < mostFrequentBird)
+    ) {
+      maxCount = birdTypes[type];
+      mostFrequentBird = type;
+    }
   }
 
-  const sortedBirds = Object.entries(birdTypes).sort(([, a], [, b]) => b - a);
-
-  return sortedBirds[0][0];
+  return mostFrequentBird;
 }
+
+// Less efficient
+// function migratoryBirds(arr) {
+//   // Write your code here
+//   let birdTypes = {};
+//   for (let type of arr) {
+//     const birdType = new String(type);
+//     if (!birdTypes.hasOwnProperty(type)) {
+//       birdTypes[type] = 0;
+//     }
+
+//     birdTypes[birdType] += 1;
+//   }
+
+//   const sortedBirds = Object.entries(birdTypes).sort(
+//     ([key1, val1], [key2, val2]) => {
+//       if (key1 === key2) {
+//         return Number(key1) - Number(key2);
+//       }
+
+//       return val2 - val1;
+//     }
+//   );
+
+//   return sortedBirds[0][0];
+// }
+// Solution end
 
 function main() {
   const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
