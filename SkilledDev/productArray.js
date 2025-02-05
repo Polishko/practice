b84bb8e3411bcfa186f13d76883105e2918b3635
Gizmo <nalanbilgin@gmail.com>
2025-02-05 08:29:16 +0200

    Added another alternative

--- SkilledDev/productArray.js
+++ SkilledDev/productArray.js
@@ -1,25 +1,51 @@
-// Alternative 2, using single product array
+// Altrenative 3, product array with division
 const buildProductArray = (input) => {
-  const results = [];
-  const len = input.length;
-  const RunningProduct = Array(len * 2).fill(1);
+  const zerosCount = 0;
+  let product = 1;
 
-  for (let i = 1; i < len; i++) {
-    RunningProduct[i] = RunningProduct[i - 1] * input[i - 1];
+  input.forEach((element) => {
+    if (element === 0) {
+      zerosCount++;
+    } else {
+      product = product * element;
+    }
+  });
 
-    const endIdx = len - i;
-    RunningProduct[len + endIdx - 1] =
-      RunningProduct[len + endIdx] * input[endIdx];
+  for (let i = 0; i < input.length; i++) {
+    if (zerosCount === 0) {
+      input[i] = product / input[i];
+    } else if (zerosCount === 1 && input[i] == 0) {
+      input[i] = product;
+    } else {
+      input[i] = 0;
+    }
   }
 
-  for (let i = 0; i < len; i++) {
-    const productExceptSelf = RunningProduct[i] * RunningProduct[len + i];
-
-    results.push(productExceptSelf);
-  }
-
-  return results;
+  return input;
 };
+
+// Alternative 2, using single product array
+// const buildProductArray = (input) => {
+//   const results = [];
+//   const len = input.length;
+//   const RunningProduct = Array(len * 2).fill(1);
+
+//   for (let i = 1; i < len; i++) {
+//     RunningProduct[i] = RunningProduct[i - 1] * input[i - 1];
+
+//     const endIdx = len - i;
+//     RunningProduct[len + endIdx - 1] =
+//       RunningProduct[len + endIdx] * input[endIdx];
+//   }
+
+//   for (let i = 0; i < len; i++) {
+//     const productExceptSelf = RunningProduct[i] * RunningProduct[len + i];
+
+//     results.push(productExceptSelf);
+//   }
+
+//   return results;
+// };
 
 // Alternative 1, O(n)
 // const buildProductArray = (input) => {
