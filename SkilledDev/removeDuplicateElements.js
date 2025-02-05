3ebcbc87fcd7b0607b30a11e3d3ab83590ba53b6
Gizmo <nalanbilgin@gmail.com>
2025-02-05 09:35:35 +0200

    Added another alternative

--- SkilledDev/removeDuplicateElements.js
+++ SkilledDev/removeDuplicateElements.js
@@ -1,19 +1,39 @@
 // Sorted array in-place duplicate removal
+// function removeDuplicates(arr) {
+//   if (arr.length === 0) return arr;
+
+//   let uniqueIndex = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== arr[uniqueIndex]) {
+//       uniqueIndex++;
+//       arr[uniqueIndex] = arr[i];
+//     }
+//   }
+
+//   arr.length = uniqueIndex + 1;
+
+//   return arr;
+// }
+
+// Un-orted array in-place duplicate removal
 function removeDuplicates(arr) {
   if (arr.length === 0) return arr;
-
+  const uniqueElements = new Set();
   let uniqueIndex = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr[uniqueIndex]) {
-      uniqueIndex++;
+    if (!uniqueElements.has(arr[i])) {
+      uniqueElements.add(arr[i]);
       arr[uniqueIndex] = arr[i];
+      uniqueIndex++;
     }
   }
 
-  arr.length = uniqueIndex + 1;
+  arr.length = uniqueIndex;
+
   return arr;
 }
 
-arr = [1, 2, 3, 4, 5, 5, 6, 7];
+arr = [1, 2, 3, 4, 5, 4, 6, 7];
 console.log(removeDuplicates(arr));
