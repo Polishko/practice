057eb5ad5c50f224b39afc9beb9315eab28378ee
Niya Mateeva Yondzhju <nalanbilgin@gmail.com>
2025-03-04 10:18:32 +0200

    Practice

--- Self/practice.js
+++ Self/practice.js
@@ -541,43 +541,43 @@ function Clock({ template }) {
 
 // class
 
-class Clock {
-  constructor({ template }) {
-    this.template = template;
-    this.timer = null;
-  }
+// class Clock {
+//   constructor({ template }) {
+//     this.template = template;
+//     this.timer = null;
+//   }
 
-  render() {
-    let date = new Date();
+//   render() {
+//     let date = new Date();
 
-    let hours = date.getHours();
-    if (hours < 10) hours = "0" + hours;
+//     let hours = date.getHours();
+//     if (hours < 10) hours = "0" + hours;
 
-    let mins = date.getMinutes();
-    if (mins < 10) mins = "0" + mins;
+//     let mins = date.getMinutes();
+//     if (mins < 10) mins = "0" + mins;
 
-    let secs = date.getSeconds();
-    if (secs < 10) secs = "0" + secs;
+//     let secs = date.getSeconds();
+//     if (secs < 10) secs = "0" + secs;
 
-    let output = this.template
-      .replace("h", hours)
-      .replace("m", mins)
-      .replace("s", secs);
+//     let output = this.template
+//       .replace("h", hours)
+//       .replace("m", mins)
+//       .replace("s", secs);
 
-    console.log(output);
-  }
+//     console.log(output);
+//   }
 
-  stop = () => {
-    clearInterval(this.timer);
-  };
+//   stop = () => {
+//     clearInterval(this.timer);
+//   };
 
-  start = () => {
-    this.render();
-    this.timer = setInterval(() => this.render(), 1000);
-  };
-}
+//   start = () => {
+//     this.render();
+//     this.timer = setInterval(() => this.render(), 1000);
+//   };
+// }
 
-let clock = new Clock({ template: "h:m:s" });
+// let clock = new Clock({ template: "h:m:s" });
 // clock.start();
 
 
@@ -595,4 +595,75 @@ class Class {
   static get foo() {
     return Class.#foo;
   } 
+}
+
+// Fix Rabbit
+
+class Animal {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name);
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
+// console.log(rabbit.name);
+
+// Extended Clock
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+// Create a new class ExtendedClock that inherits from Clock
+// and adds the parameter precision – the number of ms between “ticks”. Should be 1000 (1 second) by default.
+
+class ExtendedClock extends Clock {
+  constructor({ template, precision=1000 }) {
+    super({template});
+    this.precision = precision;
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), this.precision);
+  }
 }
